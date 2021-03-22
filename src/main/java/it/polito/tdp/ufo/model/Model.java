42d7c0940e51edca8af40c3ee132412dbad209a6
elena <elena@mbp-di-elena.lan>
2021-03-22 13:31:55 +0100

    22-03 .1

---
+++ src/main/java/it/polito/tdp/ufo/model/Model.java
@@ -0,0 +1,15 @@
+
+package it.polito.tdp.ufo.model;
+/**
+ * MODEL si intrefaccia con CONTROLLER che chiamerà i suoi metodi
+ * e con DAO quando serviranno i JDBC
+ * @author elena
+ *
+ */
+
+import java.util.List;
+
+public class Model {
+
+	private List<String> formeUFO ;
+}
